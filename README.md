f5111069a5760baad66a93f2225ca7b62a39cb05
Manish <drupalukunidevone@gmail.com>
2019-11-08 17:54:39 +0530

    Custom Batch Example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Custom-dev-drupal-8
 Custom development Drupal 8
+drupalup_service
+batch_example
---
+++ batch_example/src/Form/DeleteNodeForm.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\batch_example\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class DeleteNodeForm.
+ *
+ * @package Drupal\batch_example\Form
+ */
+class DeleteNodeForm extends FormBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'delete_node_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['delete_node'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Delete Node'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $nids = \Drupal::entityQuery('node')
+      ->condition('type', 'article')
+      ->sort('created', 'ASC')
+      ->execute();
+
+    $batch = array(
+      'title' => t('Deleting Node...'),
+      'operations' => array(
+        array(
+          '\Drupal\batch_example\DeleteNode::deleteNodeExample',
+          array($nids)
+        ),
+      ),
+      'finished' => '\Drupal\batch_example\DeleteNode::deleteNodeExampleFinishedCallback',
+    );
+
+    batch_set($batch);
+  }
+
+}
+
+
